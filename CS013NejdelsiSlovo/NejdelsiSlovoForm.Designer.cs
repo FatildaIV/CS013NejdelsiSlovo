ef6b4d5a8d4858141f571296644219fe03d4bc40
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:48:51 +0200

    Initial commit

---
+++ CS013NejdelsiSlovo/NejdelsiSlovoForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CS013NejdelsiSLovo
+{
+    partial class NejdelsiSlovoForm
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NejdelsiSlovoForm));
+            this.VstupTextBox = new System.Windows.Forms.TextBox();
+            this.NejdelsiSlovoLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // VstupTextBox
+            // 
+            resources.ApplyResources(this.VstupTextBox, "VstupTextBox");
+            this.VstupTextBox.Name = "VstupTextBox";
+            this.VstupTextBox.TextChanged += new System.EventHandler(this.VstupTextBox_TextChanged);
+            // 
+            // NejdelsiSlovoLabel
+            // 
+            resources.ApplyResources(this.NejdelsiSlovoLabel, "NejdelsiSlovoLabel");
+            this.NejdelsiSlovoLabel.Name = "NejdelsiSlovoLabel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // NejdelsiSlovoForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NejdelsiSlovoLabel);
+            this.Controls.Add(this.VstupTextBox);
+            this.Name = "NejdelsiSlovoForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox VstupTextBox;
+        private System.Windows.Forms.Label NejdelsiSlovoLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CS013NejdelsiSlovo/NejdelsiSlovoForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS013NejdelsiSLovo
+{
+    public partial class NejdelsiSlovoForm : Form
+    {
+        public NejdelsiSlovoForm()
+        {
+            InitializeComponent();
+        }
+
+        static string NejdelsiSlovo(string text)
+        {
+            string[] words = text.Split(new[] { " " }, StringSplitOptions.None);
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+            return word;
+        }
+
+        private void VstupTextBox_TextChanged(object sender, EventArgs e)
+        { 
+            NejdelsiSlovoLabel.Text = SlovoDelkaLabel(VstupTextBox.Text);
+            SlovoDelkaLabel.Text = NejdelsiSlovoLabel.Text.Length.ToString();
+        }
+    }
+}
